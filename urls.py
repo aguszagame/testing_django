4057e2b238fc60e3e4b7dd652d15f27ee196de4c
aguszagame <agustinazagame1992@gmail.com>
2022-07-25 22:20:42 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -2,10 +2,13 @@
 
 from django.urls import path
 
-from .views import CalculatorView
+from .views import MovieListView, MovieCreateView, DirectorCreateView, MovieDeleteView
 
-app_name = "calculator"
+app_name = "movies"
 
 urlpatterns = [
-    path("", CalculatorView.as_view(), name="calculator"),
+    path("", MovieListView.as_view(), name="list"),
+    path("nueva-pelicula/", MovieCreateView.as_view(), name="new_movie"),
+    path("nuevo-director/", DirectorCreateView.as_view(), name="new_director"),
+    path("eliminar-pelicula/<int:pk>", MovieDeleteView.as_view(), name="delete"),
 ]
--- views.py
+++ views.py
@@ -1,20 +1,40 @@
-"""Calculator level views"""
+"""Movies level app views"""
 
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView
 
-from .services import calculate
+from .models import Movie, Director
 
 
-class CalculatorView(TemplateView):
-    """Show a calculator via GET and calculates with POST"""
-    template_name = "l1_calculator/calculator.html"
+class MovieListView(ListView):
+    """Show all movies"""
+    template_name = "l2_movies/list.html"
+    queryset = Movie.objects.select_related("director").all()
+
+
+class MovieCreateView(CreateView):
+    """Create a new movie"""
+    model = Movie
+    fields = "__all__"
+    template_name = "l2_movies/new_movie.html"
+    success_url = reverse_lazy("movies:list")
 
     def get_context_data(self, **kwargs):
-        first_number = self.request.GET.get('first_number')
-        operation = self.request.GET.get('operation')
-        second_number = self.request.GET.get('second_number')
-
         context = super().get_context_data(**kwargs)
-        context['result'] = calculate(first_number, operation, second_number)
-
+        context["directors"] = Director.objects.all()
         return context
+
+
+class DirectorCreateView(CreateView):
+    """Create a new director"""
+    model = Director
+    fields = "__all__"
+    template_name = "l2_movies/new_director.html"
+    success_url = reverse_lazy("movies:list")
+
+
+class MovieDeleteView(DeleteView):
+    """Delete an existing movie"""
+    model = Movie
+    template_name = "l2_movies/delete.html"
+    success_url = reverse_lazy('movies:list')
